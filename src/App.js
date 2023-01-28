a7d603181e1ca1b004cef61dbe64d3a46a872559
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-28 22:32:27 +0300

    changed object proprties using useState

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ErrorExample from './tutorial/1-useState/1-error-example';
 import UseStateBasics from './tutorial/1-useState/2-useState-basics';
 import UseStateArray from './tutorial/1-useState/3-useState-array';
+import UseStateObject from './tutorial/1-useState/4-useState-object';
 
 const App = () => {
   return (
     <div className="container">
-      <UseStateArray/>
+      <UseStateObject/>
     </div>
   );
 }
