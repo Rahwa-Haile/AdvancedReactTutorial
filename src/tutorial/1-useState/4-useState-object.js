34d275b08921727c3d79566250d063c6fb0394ae
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-29 15:54:28 +0300

    setting up multiple state values

--- src/tutorial/1-useState/4-useState-object.js
+++ src/tutorial/1-useState/4-useState-object.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 const UseStateObject = () => {
-  const [person, setPerson] = React.useState({
-    name: "peter",
-    age: "25",
-    message: "random message",
-  });
+  // const [person, setPerson] = React.useState({
+  //   name: "peter",
+  //   age: "25",
+  //   message: "random message",
+  // });
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('random message')
   const changeMessage = () => {
-    setPerson({ ...person, message: "Hello World" });
+    // setPerson({ ...person, message: "Hello World" });
+    setMessage("Hello World");
   };
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-
-      <h3>{person.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button type="button" className="btn" onClick={changeMessage}>
         Change Message
       </button>
