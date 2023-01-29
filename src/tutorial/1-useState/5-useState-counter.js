152966cb7d00400d593847f3b5a96038c2888a47
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-29 18:16:09 +0300

    More complex counter using functional approach to set a value

--- src/tutorial/1-useState/5-useState-counter.js
+++ src/tutorial/1-useState/5-useState-counter.js
@@ -5,17 +5,54 @@ const UseStateCounter = () => {
     const resetValue = ()=>{
         setValue(0)
     }
+    const complexIncrease = ()=>{
+        setTimeout(()=>{
+            setValue((prevState)=>{
+                return prevState + 1
+            })
+        }, 2000)
+        
+    }
   return (
     <>
-        <section style={{margin:'4rem 0'}}>
-            <h2>Random Counter</h2>
-            <h1>{value}</h1>
-            <button type='button' className='btn' onClick={()=>setValue(value + 1)}>Increment</button>
-            <button type='button' className='btn' onClick={resetValue}>Reset</button>
-            <button type='button' className='btn' onClick={()=>setValue(value-1)}>Decrement</button>
-        </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Random Counter</h2>
+        <h1>{value}</h1>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setValue(value + 1)}
+        >
+          Increment
+        </button>
+        <button type="button" className="btn" onClick={resetValue}>
+          Reset
+        </button>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setValue(value - 1)}
+        >
+          Decrement
+        </button>
+      </section>
+
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More complex counter</h2>
+        <h1>{value}</h1>
+        <button
+          type="button"
+          className="btn"
+          onClick={complexIncrease}
+        >
+          Increment Later
+        </button>
+       
+        
+      </section>
     </>
-  )
+  );
 }
 
 export default UseStateCounter
