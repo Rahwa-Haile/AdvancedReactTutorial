d184ed8ef36cc5528e680c4a7dcf068cfb4080cd
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-30 12:25:36 +0300

    useEffect conditional

--- src/tutorial/2-useEffect/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/1-useEffect-basics.js
@@ -1,18 +1,22 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const UseEffectBasics = () => {
-  const [value, setValue] = useState(0)
-  useEffect(()=>{
-    console.log('call useEffect')
-    document.title = `New message ${value}`
-  })
-  console.log('render component')
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("call useEffect");
+    if (value > 0) {
+      document.title = `New message ${value}`;
+    }
+  });
+  console.log("render component");
   return (
     <>
       <h1>{value}</h1>
-      <button className='btn' onClick={()=>setValue(value + 1)}>Click Me</button>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click Me
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default UseEffectBasics
+export default UseEffectBasics;
